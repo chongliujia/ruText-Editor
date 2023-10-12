c6ed63ee5f5c5f667b61884f30dcb7b0e6ab40d9
Chongliu Jia <jcl314159@gmail.com>
2023-10-12 17:59:01 -0500

    update and add some functions

--- ruted/src/main.rs
+++ ruted/src/main.rs
@@ -1,12 +1,15 @@
 #![warn(clippy::all, clippy::pedantic)]
-
+mod document;
 mod editor;
 mod terminal;
+mod row;
 
 use editor::Editor;
 
-pub use terminal::Terminal;
 pub use editor::Position;
+pub use row::Row;
+pub use terminal::Terminal;
+
 
 fn main() {
     Editor::default().run();
